1db11f2c5a6a4b55e5761c34e08ea24f01012ad1
Andy Frey <andy@stuffandymakes.com>
2026-04-12 13:13:49 -0700

    Initial Commit

---
+++ Sshwitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sshwitch
+//
+//  Created by Andy Frey on 4/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sshwitch/SshwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SshwitchApp.swift
+//  Sshwitch
+//
+//  Created by Andy Frey on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SshwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
